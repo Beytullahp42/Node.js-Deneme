c546286ebee1a5f70edf9c8f60e1ef46166fcb66
beytp <beytullahpay42@gmail.com>
2025-05-31 01:54:42 +0300

    denemeler

--- dist/app.js
+++ dist/app.js
@@ -4,11 +4,18 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
+const path_1 = __importDefault(require("path"));
 const errorHandler_1 = require("./middlewares/errorHandler");
 const itemController_1 = __importDefault(require("./controllers/itemController"));
 const app = (0, express_1.default)();
 app.use(express_1.default.json());
+// Serve static files from public/
+app.use(express_1.default.static(path_1.default.join(__dirname, '../public')));
 // Routes
-app.use('/items', itemController_1.default); // Now all routes are prefixed with /items
+app.use('/items', itemController_1.default);
+// Default route to serve index.html
+app.get('/', (req, res) => {
+    res.sendFile(path_1.default.join(__dirname, '../public/index.html'));
+});
 app.use(errorHandler_1.errorHandler);
 exports.default = app;
--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,5 @@
 import express from 'express';
+import path from 'path';
 import { errorHandler } from './middlewares/errorHandler';
 import itemController from './controllers/itemController';
 
@@ -6,8 +7,16 @@ const app = express();
 
 app.use(express.json());
 
+// Serve static files from public/
+app.use(express.static(path.join(__dirname, '../public')));
+
 // Routes
-app.use('/items', itemController); // Now all routes are prefixed with /items
+app.use('/items', itemController);
+
+// Default route to serve index.html
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '../public/index.html'));
+});
 
 app.use(errorHandler);
 
